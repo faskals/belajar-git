078a753cfc1807d2640e18db4ebb7fb8be615c21
faskals <144752736+faskals@users.noreply.github.com>
2024-01-17 09:28:53 +0700

    Buat file biodata.py
    pembuatan file biodata

---
+++ biodata.py
@@ -0,0 +1,8 @@
+nama = input("Masukan nama : ")
+umur = input("Masukan umur : ")
+alamat = input("Masukan alamat : ")
+
+print("\n--Biodata--") 
+print("nama : ",nama)
+print("umur : ",umur)
+print("alamat : ",alamat)
